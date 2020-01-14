52f34e7a06d3f56c1039891a356bb29a51ade658
Abishek <grabishek96@gmail.com>
2020-01-14 14:48:22 +0530

    Create sql.py

---
+++ sql.py
@@ -0,0 +1,40 @@
+from pyspark.sql.types import StructField, StructType, StringType, LongType, DateType, DoubleType
+
+myNyseDailySchema = StructType([
+  StructField("exchange", StringType(), True),
+  StructField("stock_symbol", StringType(), True),
+  StructField("date", StringType(), False),
+  StructField("open_price", DoubleType(), False),
+  StructField("high_price", DoubleType(), False),
+  StructField("low_price", DoubleType(), False),
+  StructField("close_price", DoubleType(), False),
+  StructField("volume", LongType(), False),
+  StructField("adj_close_price", DoubleType(), False)
+])
+
+nyse_daily = spark.read.format("csv")\
+.schema(myNyseDailySchema)\
+.option("sep","\t")\
+.load("/FileStore/tables/NYSE_daily_1-cb8bb.tsv")
+
+nyse_daily.show()
+
+nyse_daily.createOrReplaceTempView("nyse_daily")
+
+nyseDividentsSchema = StructType([
+  StructField("exchange", StringType(), True),
+  StructField("stock_symbol", StringType(), True),
+  StructField("date", StringType(), False),
+  StructField("dividents", DoubleType(), False)
+])
+
+nyse_divi = spark.read.format("csv")\
+.schema(nyseDividentsSchema)\
+.option("sep", "\t")\
+.load("/FileStore/tables/NYSE_dividends_1-55339.tsv")
+
+nyse_divi.show()
+
+nyse_divi.createOrReplaceTempView("nyse_divi")
+
+problem1 = spark.sql("SELECT * from nyse_daily WHERE close_price >= 200 AND volume >= 10000000").show()
