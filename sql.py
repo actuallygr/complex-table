466247ab830abdd0d9b041ca1b6012d3147e5443
Abishek <grabishek96@gmail.com>
2020-01-14 17:05:15 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -67,34 +67,22 @@ tructType schema = DataTypes.createStructType(new StructField[] {
                 .option("multiline", true)
                 .load("src/main/resources/movie_datset.txt");
 
-        movieDf = movieDf.withColumn("decade", col("year").minus(1920).divide(10).cast(DataTypes.IntegerType));
-        
-        
-        +----+------+--------------------+-------+--------------------+-------------------+--------------------+----------+-----+-------------------+------+
-|year|length|               title|subject|               actor|            actores|            director|popularity|award|          file_name|decade|
-+----+------+--------------------+-------+--------------------+-------------------+--------------------+----------+-----+-------------------+------+
-|1990|   111|Tie Me Up! Tie Me...| Comedy|   Banderas, Antonio|    Abril, Victoria|    Almod�var, Pedro|        68|   No|   NicholasCage.png|     7|
-|1991|   113|          High Heels| Comedy|        Bos�, Miguel|    Abril, Victoria|    Almod�var, Pedro|        68|   No|   NicholasCage.png|     7|
-|1983|   104|      Dead Zone, The| Horror| Walken, Christopher|      Adams, Brooke|   Cronenberg, David|        79|   No|   NicholasCage.png|     6|
-|1979|   122|                Cuba| Action|       Connery, Sean|      Adams, Brooke|     Lester, Richard|         6|   No|    seanConnery.png|     5|
-|1978|    94|      Days of Heaven|  Drama|       Gere, Richard|      Adams, Brooke|    Malick, Terrence|        14|   No|   NicholasCage.png|     5|
-|1983|   140|           Octopussy| Action|        Moore, Roger|        Adams, Maud|          Glen, John|        68|   No|   NicholasCage.png|     6|
-|1984|   101|        Target Eagle| Action|      Connors, Chuck|        Adams, Maud|Loma, Jos� Antoni...|        14|   No|   NicholasCage.png|     6|
-|1989|    99|American Angels: ...|  Drama|   Bergen, Robert D.|       Adams, Trudy|  Sebastian, Beverly|        28|   No|   NicholasCage.png|     6|
-|1985|   104|              Subway|  Drama|Lambert, Christopher|   Adjani, Isabelle|         Besson, Luc|         6|   No|   NicholasCage.png|     6|
-|1990|   149|     Camille Claudel|  Drama|   Depardieu, G�rard|   Adjani, Isabelle|      Nuytten, Bruno|        32|   No|   NicholasCage.png|     7|
-|1982|   188| Fanny and Alexander|  Drama|     Ahlstedt, B�rje|Adolphson, Kristina|     Bergman, Ingmar|        81|  Yes|        Bergman.png|     6|
-|1982|   117|Tragedy of a Ridi...|  Drama|       Tognazzi, Ugo|       Aimee, Anouk|Bertolucci, Bernardo|        17|   No|   NicholasCage.png|     6|
-|1966|   103|     A Man & a Woman|  Drama|Trintignant, Jean...|       Aimee, Anouk|     Lelouch, Claude|        46|  Yes|   NicholasCage.png|     4|
-|1986|   112|A Man & a Woman: ...|  Drama|Trintignant, Jean...|       Aimee, Anouk|     Lelouch, Claude|        49|   No|   NicholasCage.png|     6|
-|1966|   103|Un Hombre y una M...|  Drama|Trintignant, Jean...|       Aimee, Anouk|     Lelouch, Claude|         6|  Yes|   NicholasCage.png|     4|
-|1985|   112| Official Story, The|  Drama|     Alterio, Hector|    Aleandro, Norma|        Puenzo, Luiz|        39|  Yes|   NicholasCage.png|     6|
-|1976|   150|Lindbergh Kidnapp...|  Drama|    Hopkins, Anthony|  Alexander, Denise|         Kulik, Buzz|        51|   No| AnthonyHopkins.png|     5|
-|1929|    84|           Blackmail|Mystery|       Longden, John|       Algood, Sara|   Hitchcock, Alfred|         2|   No|alfredHitchcock.png|     0|
-|1963|   109|      Donovan's Reef| Comedy|         Wayne, John|   Allen, Elizabeth|          Ford, John|        62|   No|      johnWayne.png|     4|
-|1988|   110|Tucker: The Man &...|  Drama|       Bridges, Jeff|        Allen, Joan|Coppola, Francis ...|        68|   No|   NicholasCage.png|     6|
-+----+------+--------------------+-------+--------------------+-------------------+--------------------+----------+-----+-------------------+------+
+       movieDf = movieDf.withColumn("decade", col("year").minus(1900).divide(10).cast(DataTypes.IntegerType));
+        movieDf = movieDf.drop("subject").drop("actor").drop("actores").drop("director").drop("award").drop("file_name").drop("length");
 
+        movieDf.groupBy(col("decade")).agg(max("popularity")).show();
         
-        
+        +------+---------------+
+|decade|max(popularity)|
++------+---------------+
+|     6|             88|
+|     3|             88|
+|     5|             86|
+|     9|             88|
+|     4|             88|
+|     8|             88|
+|     7|             88|
+|     2|             83|
++------+---------------+
+
 
